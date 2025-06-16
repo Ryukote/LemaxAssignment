7e71de3375a810cbe07c0631c230f253dbd3cc30
Antonio Halužan <ryukote@outlook.com>
2025-06-16 14:32:04 +0200

    Added validation tests

---
+++ backend/HotelSearch/HotelSearch.Tests/AddUpdateHotelRequestValidatorTests.cs
@@ -0,0 +1,85 @@
+﻿using FluentValidation.TestHelper;
+using HotelSearch.Application.Validations;
+using HotelSearch.Service.Payloads.Requests;
+
+namespace HotelSearch.Test.Validators
+{
+    public class AddUpdateHotelRequestValidatorTests
+    {
+        private readonly AddUpdateHotelRequestValidator _validator = new();
+
+        [Fact]
+        public void Should_Have_Error_When_Name_Is_Empty()
+        {
+            var model = new AddUpdateHotelRequest
+            {
+                Name = "",
+                Price = 100,
+                Latitude = 45,
+                Longitude = 15
+            };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Price_Is_Negative()
+        {
+            var model = new AddUpdateHotelRequest
+            {
+                Name = "Test",
+                Price = -10,
+                Latitude = 45,
+                Longitude = 15
+            };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Price);
+        }
+
+        [Theory]
+        [InlineData(-91)]
+        [InlineData(91)]
+        public void Should_Have_Error_When_Latitude_Is_Invalid(decimal latitude)
+        {
+            var model = new AddUpdateHotelRequest
+            {
+                Name = "Test",
+                Price = 100,
+                Latitude = latitude,
+                Longitude = 15
+            };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Latitude);
+        }
+
+        [Theory]
+        [InlineData(-181)]
+        [InlineData(181)]
+        public void Should_Have_Error_When_Longitude_Is_Invalid(decimal longitude)
+        {
+            var model = new AddUpdateHotelRequest
+            {
+                Name = "Test",
+                Price = 100,
+                Latitude = 45,
+                Longitude = longitude
+            };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Longitude);
+        }
+
+        [Fact]
+        public void Should_Not_Have_Any_Errors_When_Request_Is_Valid()
+        {
+            var model = new AddUpdateHotelRequest
+            {
+                Name = "Valid Hotel",
+                Price = 150,
+                Latitude = 45.5m,
+                Longitude = 16.3m
+            };
+            var result = _validator.TestValidate(model);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
---
+++ backend/HotelSearch/HotelSearch.Tests/PaginateHotelRequestValidatorTests.cs
@@ -0,0 +1,87 @@
+﻿using FluentValidation.TestHelper;
+using HotelSearch.Application.Payloads.Requests;
+using HotelSearch.Application.Validations;
+
+namespace HotelSearch.Test.Validators
+{
+    public class PaginateHotelRequestValidatorTests
+    {
+        private readonly PaginateHotelRequestValidator _validator = new();
+
+        [Fact]
+        public void Should_Have_Error_When_PageNumber_Is_Less_Than_1()
+        {
+            var model = new PaginateHotelRequest
+            {
+                PageNumber = 0,
+                PageSize = 10,
+                UserLatitude = 45,
+                UserLongitude = 15
+            };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.PageNumber);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(101)]
+        public void Should_Have_Error_When_PageSize_Is_Out_Of_Range(int pageSize)
+        {
+            var model = new PaginateHotelRequest
+            {
+                PageNumber = 1,
+                PageSize = pageSize,
+                UserLatitude = 45,
+                UserLongitude = 15
+            };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.PageSize);
+        }
+
+        [Theory]
+        [InlineData(-91)]
+        [InlineData(91)]
+        public void Should_Have_Error_When_Latitude_Is_Invalid(double latitude)
+        {
+            var model = new PaginateHotelRequest
+            {
+                PageNumber = 1,
+                PageSize = 10,
+                UserLatitude = latitude,
+                UserLongitude = 15
+            };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.UserLatitude);
+        }
+
+        [Theory]
+        [InlineData(-181)]
+        [InlineData(181)]
+        public void Should_Have_Error_When_Longitude_Is_Invalid(double longitude)
+        {
+            var model = new PaginateHotelRequest
+            {
+                PageNumber = 1,
+                PageSize = 10,
+                UserLatitude = 45,
+                UserLongitude = longitude
+            };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.UserLongitude);
+        }
+
+        [Fact]
+        public void Should_Not_Have_Any_Errors_When_Request_Is_Valid()
+        {
+            var model = new PaginateHotelRequest
+            {
+                PageNumber = 2,
+                PageSize = 20,
+                UserLatitude = 44.9d,
+                UserLongitude = 13.2d
+            };
+            var result = _validator.TestValidate(model);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
